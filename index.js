e477366b6b5f582d3dba121cf7f61de8a6951afb
sione-tenifa <johntenifa@yahoo.com>
2022-11-07 17:58:18 -0700

    added buttons

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-let randomDiceImg = "dice" + randomNumber1 + ".png"
-let randomImgSource2 = "images/dice" + randomNumber2 + ".png"
-let randomImageSource = "images/" + randomDiceImg
+const playGame = () => {
+    let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-let img1 = document.querySelectorAll("img")[0]
-let img2 = document.querySelectorAll("img")[1]
+    let randomDiceImg = "dice" + randomNumber1 + ".png"
+    let randomImgSource2 = "images/dice" + randomNumber2 + ".png"
+    let randomImageSource = "images/" + randomDiceImg
 
-img1.setAttribute("src", randomImageSource)
-img2.setAttribute("src", randomImgSource2)
+    let img1 = document.querySelectorAll("img")[0]
+    let img2 = document.querySelectorAll("img")[1]
 
-if (randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerHTML = "Player 1 Won!👏"
-}else if( randomNumber2 > randomNumber1){
-    document.querySelector("h1").innerHTML = "Player 2 Won!👏"
-}else{
-    document.querySelector("h1").innerHTML = "Draw"
+    img1.setAttribute("src", randomImageSource)
+    img2.setAttribute("src", randomImgSource2)
 
+    if (randomNumber1 > randomNumber2){
+        document.querySelector("h1").innerHTML = "👏Player 1 Won!"
+    }else if( randomNumber2 > randomNumber1){
+        document.querySelector("h1").innerHTML = "Player 2 Won!👏"
+    }else{
+        document.querySelector("h1").innerHTML = "Draw"
+    }
+}
+
+const refresh = () => {
+    location.reload();
 }
